f2eabfd7d34a596782f53720af89fe47fd2fb1af
Shendelzare <s10bh4n@gmail.com>
2021-12-03 20:55:04 +0100

    1. Posicionamiento de piezas: el sistema debe identificar cuando una posición de una torre resulta válida o inválida y colocar la pieza en el tablero en su caso. 	Se añaden Test que fallan

--- P6-TDD/test/game/JuegoTest.java
+++ P6-TDD/test/game/JuegoTest.java
@@ -2,17 +2,47 @@ package game;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class JuegoTest {
 
+private Juego juego;
+	
+	@Before
+	public void setUp() {
+		juego = new Juego();
+	}
 	/*
-	 * Este test falla siempre. Debe eliminarse e incorporarse, de forma ordenada los test que se vayan diseñando desde los
-	 * requisitos propuestos.
+	 * Tests para requisito 1
 	 */
 	@Test
-	public void testJugar() {
-		fail("Not yet implemented");
+	public void testGetTablero() {
+		
+		assertNotNull(juego.getTablero());
 	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testPosicionarFueraTableroArriba() {
+		juego.jugar(1, 5);
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void testPosicionarFueraTableroAbajo() {
+		juego.jugar(1, 0);
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void testPosicionarFueraTableroDerecha() {
+		juego.jugar(5, 1);
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void testPosicionarFueraTableroIzquierda() {
+		juego.jugar(0, 1);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testIsCasillaVacia() {
+		juego.jugar(3, 1);
+	}
+	
 
 }
