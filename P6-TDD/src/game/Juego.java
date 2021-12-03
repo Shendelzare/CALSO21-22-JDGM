2721055c91bec9b01b7e0ff2491b3b13caf6dece
Shendelzare <s10bh4n@gmail.com>
2021-12-03 21:00:03 +0100

    Se ha añadido una matriz de tablero con tamaño 4 y el getter/setter  - Primer Test pasado

--- P6-TDD/src/game/Juego.java
+++ P6-TDD/src/game/Juego.java
@@ -1,6 +1,5 @@
 package game;
 
-
 /*
  * Clase Juego tiene la responsabilidad de ofrecer las funcionalidades básicas del juego de tablero descrito en
  * el enunciado de la práctica. Inicialmente no se implementa funcionalidad alguna y el estado de la programación de la clase es inicial
@@ -11,14 +10,31 @@ package game;
 
 public class Juego {
 	
+	private int[][] tablero;
+	private static final int TAMANO_TABLERO = 4;
+
+
+	public Juego() {
+		tablero = new int[TAMANO_TABLERO][TAMANO_TABLERO];
+	}
+
+	public int[][] getTablero() {
+		return tablero;
+	}
+
+	public void setTablero(int[][] tablero) {
+		this.tablero = tablero;
+	}
 	/*
-	 * Método de inicio asociado al requisito 1 donde se implementa la funcionalidad básica de posicionar una pieza en el tablero.
+	 * Método de inicio asociado al requisito 1 donde se implementa la funcionalidad
+	 * básica de posicionar una pieza en el tablero.
 	 * 
 	 * @param x fila de la casilla en la que se desea incorporar la pieza
-	 * @param y columna de la casilla en la que se desea indorporar la pieza 
+	 * 
+	 * @param y columna de la casilla en la que se desea indorporar la pieza
 	 */
 	public void jugar(int x, int y) {
-		
+
 	}
 
 }
