1ae2ca0778dbdc3198f34497d42e2665fc6ea37f
Shendelzare <s10bh4n@gmail.com>
2021-12-03 21:36:04 +0100

    2. Dos jugadores: el sistema debe dar soporte a que participen dos jugadores de forma alternada independientemente de quien comience la partida.  - Se añaden Test para que jueguen los jugadores y se inicia el turno

--- P6-TDD/test/game/JuegoTest.java
+++ P6-TDD/test/game/JuegoTest.java
@@ -7,42 +7,69 @@ import org.junit.Test;
 
 public class JuegoTest {
 
-private Juego juego;
-	
+	private Juego juego;
+
 	@Before
 	public void setUp() {
 		juego = new Juego();
 	}
+
 	/*
 	 * Tests para requisito 1
 	 */
 	@Test
 	public void testGetTablero() {
-		
+
 		assertNotNull(juego.getTablero());
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testPosicionarFueraTableroArriba() {
 		juego.jugar(1, 5);
 	}
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testPosicionarFueraTableroAbajo() {
 		juego.jugar(1, 0);
 	}
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testPosicionarFueraTableroDerecha() {
 		juego.jugar(5, 1);
 	}
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testPosicionarFueraTableroIzquierda() {
 		juego.jugar(0, 1);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testIsCasillaVacia() {
 		juego.jugar(3, 1);
 	}
-	
 
+	@Test
+	public void iniciaJuego() {
+		juego = new Juego(JugadoresEnum.BLANCAS);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void jugarNegras() {
+		juego.jugar(JugadoresEnum.NEGRAS, 2, 3);
+	}
+
+	@Test
+	public void jugarNegras1() {
+		juego.jugar(JugadoresEnum.NEGRAS, 2, 3);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void jugarBlancas() {
+		juego.jugar(JugadoresEnum.BLANCAS, 2, 3);
+	}
+
+	@Test
+	public void jugarBlancas1() {
+		juego.jugar(JugadoresEnum.BLANCAS, 2, 3);
+	}
 }
