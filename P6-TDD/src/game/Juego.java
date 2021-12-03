cab94062f159d0c1c11381cc82b0a80a6909449a
Shendelzare <s10bh4n@gmail.com>
2021-12-03 20:51:22 +0100

    Commit inicial

---
+++ P6-TDD/src/game/Juego.java
@@ -0,0 +1,24 @@
+package game;
+
+
+/*
+ * Clase Juego tiene la responsabilidad de ofrecer las funcionalidades básicas del juego de tablero descrito en
+ * el enunciado de la práctica. Inicialmente no se implementa funcionalidad alguna y el estado de la programación de la clase es inicial
+ * No se recogen ni estructras de datos ni métodos que deberán incorprarse de forma progresiva.
+ * 
+ * IMPORTANTE: recordad el ciclo TDD: Test->Código->Refactorización->Test->...
+ */
+
+public class Juego {
+	
+	/*
+	 * Método de inicio asociado al requisito 1 donde se implementa la funcionalidad básica de posicionar una pieza en el tablero.
+	 * 
+	 * @param x fila de la casilla en la que se desea incorporar la pieza
+	 * @param y columna de la casilla en la que se desea indorporar la pieza 
+	 */
+	public void jugar(int x, int y) {
+		
+	}
+
+}
---
+++ P6-TDD/test/game/JuegoTest.java
@@ -0,0 +1,18 @@
+package game;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JuegoTest {
+
+	/*
+	 * Este test falla siempre. Debe eliminarse e incorporarse, de forma ordenada los test que se vayan diseñando desde los
+	 * requisitos propuestos.
+	 */
+	@Test
+	public void testJugar() {
+		fail("Not yet implemented");
+	}
+
+}
